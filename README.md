9b0dc4b313d6a00b8974fc474de1e402287ec07e
amuthan-sakthivel <amuthan.sakthivel@clipboardhealth.com>
2023-05-12 11:26:55 +0530

    integrated with allure

---
+++ README.md
@@ -0,0 +1,11 @@
+**Selenide + Junit5 + Allure**
+
+[Video Tutorials about how to create this framework](https://www.youtube.com/watch?v=5vrYMfsxkGY&list=PL9ok7C7Yn9A9YyRISFrxHdaxb5qqrxp_i)
+
+**To run tests:**
+
+```mvn clean test```
+
+To generate allure report after execution
+
+```allure serve ./target/allure-results```
--- src/test/java/com/orangehrm/tests/base/TestSetup.java
+++ src/test/java/com/orangehrm/tests/base/TestSetup.java
@@ -1,10 +1,18 @@
 package com.orangehrm.tests.base;
 
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 
 import static com.codeborne.selenide.Selenide.open;
 
 public class TestSetup {
+
+  @BeforeAll
+  static void setupAllureReports() {
+    SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+  }
 
   @BeforeEach
   void setUp() {
