b087403f792145d3fe32aa437827acdb29dcf051
amuthan-sakthivel <amuthan.sakthivel@clipboardhealth.com>
2023-09-03 21:06:22 +0530

    feat: add get user test

---
+++ src/main/java/com/tmb/api/UserApi.java
@@ -0,0 +1,26 @@
+package com.tmb.api;
+
+
+import com.tmb.entity.requests.UserDetails;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+
+import static io.restassured.RestAssured.given;
+
+public class UserApi {
+    private UserApi() {
+    }
+
+    public static Response createUser(UserDetails userDetails) {
+        return given()
+                .body(userDetails)
+                .contentType(ContentType.JSON)
+                .post("/api/users");
+    }
+
+    public static Response getUser(int userId) {
+    return given()
+            .pathParam("userId", userId)
+            .get("/api/users/{userId}");
+    }
+}
---
+++ src/test/java/com/tmb/tests/api/UserTest.java
@@ -0,0 +1,47 @@
+package com.tmb.tests.api;
+
+import com.tmb.api.UserApi;
+import com.tmb.entity.response.CreateUserResponse;
+import com.tmb.entity.requests.UserDetails;
+import com.tmb.tags.testtype.ApiTest;
+import com.tmb.tags.suite.RegressionTest;
+import com.tmb.testdata.UserTestData;
+import com.tmb.tests.base.ApiTestSetUp;
+import io.restassured.response.Response;
+
+import static com.tmb.asserthelpers.ResponseAssert.*;
+
+@RegressionTest
+class UserTest extends ApiTestSetUp {
+
+    private final UserDetails userDetails = UserTestData.getUserDetails();
+
+    //SDET essentials - Testing mini bytes
+    @ApiTest
+    void createUser() {
+
+        Response response = UserApi.createUser(userDetails);
+
+        assertThat(response)
+                .statusCodeIs(201)
+                .canBeDeserializedTo(CreateUserResponse.class)
+                .hasKeyWithValue("job", userDetails.getJob())
+                .matchingRule(e -> e.jsonPath().getString("name").equalsIgnoreCase(userDetails.getName()))
+                .matchesSchemaInFile("create-user-response-schema.json")
+                .assertAll(); //don't forget to call assertAll
+    }
+
+    @ApiTest
+    void getUserDetails() {
+        Response response = UserApi.getUser(2);
+
+        assertThat(response)
+                .statusCodeIs(200)
+                .matchesSchemaInFile("get-user-response-schema.json")
+                .matchingRule(response1 ->
+                        response1.jsonPath().getString("data.email").equalsIgnoreCase("janet.weaver@reqres.in"))
+                .assertAll();
+    }
+
+
+}
