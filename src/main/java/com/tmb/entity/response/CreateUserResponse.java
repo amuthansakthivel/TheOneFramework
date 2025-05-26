4e26fff56e0948453ace9f7d41834e7f22635755
Gaurav Khurana <gaurav.jaz@gmail.com>
2025-05-26 10:08:56 +0530

    feat: implement delete user API and enhance user details constructor; add tests for user creation and deletion scenarios (#50)
    Co-authored-by: Gaurav Khurana <C151709@constellation.com>

--- src/main/java/com/tmb/entity/response/CreateUserResponse.java
+++ src/main/java/com/tmb/entity/response/CreateUserResponse.java
@@ -2,6 +2,23 @@ package com.tmb.entity.response;
 
 import lombok.Data;
 
+/**
+ * The CreateUserResponse class represents the response received after creating a user.
+ * It contains information about the user such as their name, job, creation timestamp, and ID.
+ * 
+ * <p>This class uses Lombok's @Data annotation to automatically generate 
+ * boilerplate code such as getters, setters, toString, equals, and hashCode methods.</p>
+ * 
+ * <p>Fields:</p>
+ * <ul>
+ *   <li>{@code name} - The name of the user.</li>
+ *   <li>{@code job} - The job title of the user.</li>
+ *   <li>{@code createdAt} - The timestamp when the user was created.</li>
+ *   <li>{@code id} - The unique identifier of the user.</li>
+ * </ul>
+ * 
+ * @see lombok.Data
+ */
 @Data
 public class CreateUserResponse {
   private String name;
